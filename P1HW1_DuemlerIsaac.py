cfc435503cfd6fcd387e3caa0684ec23c46711e3
isaac2145 <duemleri3360@student.faytechcc.edu>
2025-06-16 17:51:41 -0400

    Add files via upload
    first

---
+++ P1HW1_DuemlerIsaac.py
@@ -0,0 +1,21 @@
+# Isaac Duemler
+# 06/16/2025
+# P1HW1
+# mathematical expressions
+
+print("Enter an integer as the base value:", end=" ")
+base = int(input())
+print("Enter an integer as the exponent:", end=" ")
+exponent = int(input())
+total1 = base**exponent
+print(base, "raised to the power of", exponent, "is", total1, "!!")
+
+print("Enter a starting integer:", end=" ")
+start = int(input())
+print("Enter an integer to add:", end=" ")
+add = int(input())
+print("Enter an integer to subtract:", end=" ")
+sub = int(input())
+total2 = start+add-sub
+print(start, "+", add, "-", sub, "is equal to", total2)
+
