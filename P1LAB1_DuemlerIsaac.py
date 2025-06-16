573f019b0b40fa98779983749d24d1bab523934a
isaac2145 <duemleri3360@student.faytechcc.edu>
2025-06-16 17:23:02 -0400

    Add files via upload

---
+++ P1LAB1_DuemlerIsaac.py
@@ -0,0 +1,9 @@
+# Isaac Duemler
+# 06/16/2025
+# P1LAB1
+# Intro to Python
+print("first name:", end=" ")
+first_name = input()
+print("last name:", end=" ")
+last_name = input()
+print("Hello,", first_name, last_name, "! Welcome to CTI-110")
