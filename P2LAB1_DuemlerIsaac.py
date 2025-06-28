5804c908c12661c64d52d00fc2d2e3e05ad9cabe
isaac2145 <duemleri3360@student.faytechcc.edu>
2025-06-27 21:54:43 -0400

    first

---
+++ P2LAB1_DuemlerIsaac.py
@@ -0,0 +1,12 @@
+#Isaac Duemler
+#06/27/2025
+#P2LAB1
+#Circle Formulas
+print("What is the radius of the circle?", end=" ")
+radius = float(input())
+diameter = radius * 2
+circumference = radius * 2 * 3.14
+area = radius**2 * 3.14159
+print(f"The diameter of the circle is {diameter:.1f}")
+print(f"The circumference of the circle is {circumference:.2f}")
+print(f"The area of the circle is {area:.3f}")
