226d25f126f07c92ac4c31774bf0d5ecb43e8e70
isaac2145 <duemleri3360@student.faytechcc.edu>
2025-07-13 17:57:01 -0400

    first

---
+++ P4LAB1a_DuemlerIsaac.py
@@ -0,0 +1,13 @@
+import turtle
+
+num = 1
+num2 = 1
+while num<=4:
+    turtle.forward(50)
+    turtle.left(90)
+    num = num + 1
+while num2<4:
+    turtle.forward(50)
+    turtle.left(120)
+    num2 = num2 + 1
+    
---
+++ P4LAB1b_DuemlerIsaac.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.circle(50, 180)
